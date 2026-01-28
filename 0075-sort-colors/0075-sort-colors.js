222d135913e82442eb6b512f711205c6034c2b3e
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-01-28 12:07:46 +0530

    Time: 0 ms (100%), Space: 54.4 MB (33.02%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+let low=0,mid=0,high=nums.length-1;
+while(mid <= high){
+    if(nums[mid]===0){
+        [nums[low],nums[mid]] = [nums[mid],nums[low]];
+        mid++;
+        low++
+    }else if(nums[mid]===1){
+        mid++;
+    }else{
+        [nums[mid],nums[high]]=[nums[high],nums[mid]]
+        high--;
+      
+    }
+}
+return nums
+};
