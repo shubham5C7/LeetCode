4a6370d533da42c47eed26a4fe0eac42d026e80a
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-01-29 20:00:32 +0530

    Time: 0 ms (100%), Space: 54.4 MB (12.67%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if(n<=2) return n
+    let first=1,second=2;
+    for(let i=3;i<=n;i++){
+        let third=first+second;
+        first=second;
+        second=third;
+    }
+    return second
+};
