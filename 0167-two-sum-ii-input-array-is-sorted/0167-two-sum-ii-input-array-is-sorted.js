d954ae865f197fe3f7eb2369dc14e516b9fc72aa
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-01-29 10:28:01 +0530

    Time: 6 ms (10.59%), Space: 55.6 MB (52.76%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let mpp={};
+    for(let i=0;i<numbers.length;i++){
+        let third = target-numbers[i];
+        if(third in mpp){
+            return [mpp[third]+1,i+1]
+        }
+        mpp[numbers[i]]=i;
+    }
+};
