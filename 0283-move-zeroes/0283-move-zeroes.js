30ae5bb23984739f123bab1553d1de571014aa0e
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-02-16 11:34:30 +0530

    Time: 4 ms (29.42%), Space: 60.7 MB (40.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,12 +3,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let left =0;
-    for(let right=0;right<nums.length;right++){
+    let n=nums.length;
+    let left=0;
+    for(let right=0;right<n;right++){
         if(nums[right] != 0){
-     [nums[left],nums[right]] = [nums[right],nums[left]]
-    left++;
+            [nums[left],nums[right]]=[nums[right],nums[left]];
+            left++;
         }
     }
-    return nums
+    return  nums;
 };
