fc2fe2ba29760237159ccb852a5e368b589325b8
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-02-19 23:20:27 +0530

    Time: 0 ms (100%), Space: 55.9 MB (50.51%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack=[];
+    let map={
+        "(":")",
+        "[":"]",
+        "{":"}"
+    }
+    for(let ch of s ){
+        if(map[ch]){
+            stack.push(ch)
+        }else{
+            let top=stack.pop();
+
+            if(map[top]!==ch){
+                return false;
+            }
+        }
+    }
+     return stack.length === 0;
+};
