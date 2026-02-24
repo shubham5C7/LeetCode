774e1a9faff4c0579f0bf5449ebca88925f1a19e
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-02-24 22:14:48 +0530

    Time: 5 ms (81.74%), Space: 64.9 MB (88.7%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.js
+++ 0326-power-of-three/0326-power-of-three.js
@@ -3,8 +3,10 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    if(n<=0) return false;
+       if(n<=0) return false;
     if(n===1) return true;
-    if(n%3===1) return false;
-    return isPowerOfThree(n/3)
+    if(n%3!=0){
+        return false;
+    }
+    return isPowerOfThree(n/3);
 };
