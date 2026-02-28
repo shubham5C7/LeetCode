0e23ee23ba2ad6ec574e651c7e2f5cd907e8962d
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-02-28 22:30:50 +0530

    Time: 0 ms (100%), Space: 53.8 MB (65.06%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -3,15 +3,35 @@
  * @param {number} target
  * @return {boolean}
  */
+/* 
+[1,3,5,7]
+[10,11,16,20]
+[23,30,34,60]
+n= 3
+m = 4
+
+row=0, col=3  target =13
+if(0<3 && 4 >= 0){
+    if(mat[row][col]=== target  )
+    7 === 13 -> false
+    7 > 13  --> false 
+    row++ row =1
+
+    1<3 && 3>=0{
+        20 > 10 col--  col=2
+    }
+}
+*/
+
 var searchMatrix = function(matrix, target) {
-    let n=matrix.length;
-    let m=matrix[0].length;
-    for(let i=0;i<n;i++){
-        for(let j=0;j<m;j++){
-            if(matrix[i][j]===target){
-                return true
-            }
-        }
+  let rows = matrix.length;
+    let cols = matrix[0].length;
+
+    let row=0,col=cols-1;
+    while(row< rows && col>=0){
+     if(matrix[row][col] === target) return true;
+     else if(matrix[row][col] > target) col--;
+     else row++
     }
     return false;
 };
