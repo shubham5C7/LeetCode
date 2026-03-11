8cfbe54f229872fd64a2dfdf79a5b7636a4afbd7
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-03-11 23:28:44 +0530

    Time: 18 ms (39.77%), Space: 56.2 MB (96.88%) - LeetHub

---
+++ JavaScript/0018-4sum/0018-4sum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+    let n=nums.length;
+    let res=[];
+    nums.sort((a,b)=>a-b);
+    for(let i=0;i<n-1;i++){
+        if(i>0 && nums[i]===nums[i-1]) continue;;
+        for(let j=i+1;j<n-2;j++){
+            if(j>i+1 && nums[j]===nums[j-1]) continue;
+            let left=j+1;
+            let right=n-1;
+            while(left < right){
+                let sum = nums[i]+nums[j]+nums[left]+nums[right];
+
+                if(sum < target){
+                    left++;
+                }else if(sum > target){
+                    right--;
+                }else{
+                    res.push([nums[i],nums[j],nums[left],nums[right]]);
+                    left++;
+                    right--;
+                    while(left < right && nums[left] === nums[left-1]) left++;
+                     while(left < right && nums[right] === nums[right+1]) right--;
+                }
+            }
+        }
+    }
+    return res;
+};
