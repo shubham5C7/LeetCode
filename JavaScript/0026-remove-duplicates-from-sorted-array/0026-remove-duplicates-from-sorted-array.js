1d51490c9d5ff05e20aa37c0d30bfba43f1a0c37
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-03-11 23:37:42 +0530

    Time: 1 ms (49.13%), Space: 58.1 MB (57.37%) - LeetHub

--- JavaScript/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ JavaScript/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let k=0;
-    for(let i=0;i<nums.length;i++){
-        if(nums[i] != nums[k]){
-            k++
-            nums[k] = nums[i];
-        }
+let k=0;
+for(let i=0;i<nums.length;i++){
+    if(nums[i] !==nums[k]){
+        k++
+        nums[k]=nums[i]
     }
-    return k+1
+}
+return k+1
 };
