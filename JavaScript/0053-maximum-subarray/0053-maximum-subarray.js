06504e579ebeed05fc7be08b2d1953edecb7f8b7
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-03-12 12:35:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ JavaScript/0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let curr=nums[0];
+    let sum=nums[0];
+    for(let i=1;i<nums.length;i++){
+        curr=Math.max(nums[i],curr+nums[i]);
+        sum=Math.max(sum,curr)
+    }
+    return sum
+};
