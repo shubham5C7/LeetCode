28d3c8932f95473468313865f4811e34abd0c718
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-03-12 18:03:24 +0530

    Time: 1 ms (13.21%), Space: 55.9 MB (5.68%) - LeetHub

---
+++ JavaScript/0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+for(let num of nums){
+    if(num === target){
+        return true
+    }
+}
+return false;
+};
