3bafd73783b626422508e82196eecaafd90032b4
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-03-12 11:51:44 +0530

    Time: 2 ms (84.32%), Space: 63.3 MB (63.74%) - LeetHub

---
+++ JavaScript/0041-first-missing-positive/0041-first-missing-positive.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    let n=nums.length;
+    for(let i=0;i<n;i++){
+        while(nums[i] > 0 && nums[i] <= n && nums[nums[i]-1] !== nums[i]){
+            let crtIdx =nums[i]-1;
+            [nums[i],nums[crtIdx]] = [nums[crtIdx],nums[i]];
+        }
+    }
+    for(let i=0;i<n;i++){
+        if(nums[i] !==i+1){
+            return i+1
+        }
+    }
+    return n+1
+};
