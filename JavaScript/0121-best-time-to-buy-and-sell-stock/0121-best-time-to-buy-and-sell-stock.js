d360c78d3b06611df8c35737a36faf20930f8420
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-03-26 20:03:58 +0530

    Time: 1 ms (94.95%), Space: 64 MB (97.69%) - LeetHub

--- JavaScript/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ JavaScript/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,14 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let n=prices.length;
-    let maxi=0;
-    let minidiff = Infinity;
-    for(let i=0;i<n;i++){
-       if(prices[i] < minidiff){
-       minidiff=prices[i]
+      let maxi=0;
+       let minidiff=Infinity;
+       for(let i=0;i<prices.length;i++){
+        if(prices[i] < minidiff){
+            minidiff = prices[i];
+        }
+        maxi=Math.max(maxi,prices[i]-minidiff)
        }
-       maxi=Math.max(maxi,prices[i]-minidiff)
-    }
-    return maxi
+       return maxi
 };
