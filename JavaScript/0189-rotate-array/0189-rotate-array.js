71781451f9540de465d43be64bc9d54f9a9622fc
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-03-26 19:31:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ JavaScript/0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ const reverse = function(nums,left,right){
+    while(left < right){
+        [nums[left],nums[right]] = [nums[right],nums[left]];
+        left++;
+        right--;
+    }
+ };
+var rotate = function(nums, k) {
+    let n=nums.length;
+    k=k%n
+    reverse(nums,0,n-1);
+    reverse(nums,0,k-1);
+     reverse(nums,k,n-1);
+};
