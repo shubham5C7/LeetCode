4ccc843a6ec67b35f82ede6edacc95791e14b28d
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-03-31 13:11:18 +0530

    Time: 0 ms (100%), Space: 53.2 MB (91.97%) - LeetHub

--- JavaScript/0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ JavaScript/0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -9,9 +9,10 @@ var searchMatrix = function(matrix, target) {
 
     let row=0,col=m-1;
     while(row < n && col >= 0){
-        if(matrix[row][col]===target) return true
-        else if(matrix[row][col] > target) col--;
-        else row++;
+        if(matrix[row][col] === target) return true;
+        else if(matrix[row][col] < target) row++;
+        else col--
+
     }
     return false
 };
