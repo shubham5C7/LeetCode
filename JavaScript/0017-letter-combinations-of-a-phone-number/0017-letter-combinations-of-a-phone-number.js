c01d09131dd89a59783cc56f289c6f9300459aa9
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-03-31 20:58:49 +0530

    Time: 0 ms (100%), Space: 53.9 MB (31.23%) - LeetHub

---
+++ JavaScript/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    if(digits.length === 0) return;
+    let map ={
+    '2':"abc",
+    '3':"def",
+    '4':"ghi",
+    '5':"jkl",
+    '6':"mno",
+    '7':"pqrs",
+    '8':"tuv",
+    '9':"wxyz"
+};
+let res=[];
+
+function backtracking(idx,path){
+if(idx === digits.length){
+    res.push(path);
+    return;
+}
+
+let letters = map[digits[idx]];
+for(let ch of letters){
+    backtracking(idx+1,path+ch)
+}
+}
+backtracking(0,"");
+return res;
+};
