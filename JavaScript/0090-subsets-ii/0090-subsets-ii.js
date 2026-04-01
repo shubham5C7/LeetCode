055ba41e9ba288f98077b1b2a7c6c41e80d36154
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-04-01 10:30:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- JavaScript/0090-subsets-ii/0090-subsets-ii.js
+++ JavaScript/0090-subsets-ii/0090-subsets-ii.js
@@ -3,17 +3,15 @@
  * @return {number[][]}
  */
 var subsetsWithDup = function(nums) {
-    nums.sort((a,b)=>a-b)
+    nums.sort((a,b)=>a-b);
     let res=[];
 function backtrack(idx,path){
-
-        res.push([...path]);
-
+res.push([...path]);
 for(let i=idx;i<nums.length;i++){
-if(i>idx && nums[i] === nums[i-1]) continue;
-path.push(nums[i]);
-backtrack(i+1,path)
-path.pop()
+    if(i > idx && nums[i] === nums[i-1]) continue;
+    path.push(nums[i]);
+    backtrack(i+1,path);
+    path.pop()
 }
 }
 backtrack(0,[])
