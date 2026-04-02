e1969b847c04719883a004c685a2eb78944c1e93
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-04-02 14:22:47 +0530

    Time: 6 ms (31.28%), Space: 57.6 MB (77.95%) - LeetHub

---
+++ JavaScript/0032-longest-valid-parentheses/0032-longest-valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function(s) {
+    if(s.length === 0) return 0
+let stack=[-1];
+let maxLen=0;
+for(let i=0;i<s.length;i++){
+    if(s[i] === "("){
+        stack.push(i)
+    }else{
+        stack.pop();
+
+        if(stack.length===0){
+            stack.push(i)
+        }else{
+            let len = i - stack[stack.length-1];
+            maxLen = Math.max(maxLen,len)
+        }
+    }
+}
+return maxLen
+};
