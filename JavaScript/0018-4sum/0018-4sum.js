63450541049d6823297caacc90044972b1e4579a
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-04-02 16:01:34 +0530

    Time: 17 ms (52.82%), Space: 57.4 MB (66.26%) - LeetHub

--- JavaScript/0018-4sum/0018-4sum.js
+++ JavaScript/0018-4sum/0018-4sum.js
@@ -5,30 +5,29 @@
  */
 var fourSum = function(nums, target) {
     let n=nums.length;
-    let res=[];
     nums.sort((a,b)=>a-b);
-    for(let i=0;i<n-1;i++){
-        if(i>0 && nums[i]===nums[i-1]) continue;;
-        for(let j=i+1;j<n-2;j++){
-            if(j>i+1 && nums[j]===nums[j-1]) continue;
-            let left=j+1;
-            let right=n-1;
+    let res=[];
+    for(let i=0;i<n;i++){
+        if(i>0 && nums[i]=== nums[i-1]) continue;
+        for(let j=i+1;j<n-1;j++){
+            if(j>i+1 && nums[j] === nums[j-1]) continue;
+            let left=j+1,right=n-1;
             while(left < right){
                 let sum = nums[i]+nums[j]+nums[left]+nums[right];
 
-                if(sum < target){
-                    left++;
-                }else if(sum > target){
+                if(sum > target){
                     right--;
+                }else if(sum < target){
+                    left++
                 }else{
                     res.push([nums[i],nums[j],nums[left],nums[right]]);
                     left++;
                     right--;
                     while(left < right && nums[left] === nums[left-1]) left++;
-                     while(left < right && nums[right] === nums[right+1]) right--;
+                    while(left < right && nums[right] === nums[right+1]) right--;
                 }
             }
         }
     }
-    return res;
+    return res
 };
