ed733d96066080290c846b890be9852bcc22a254
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-04-05 21:01:22 +0530

    Time: 2 ms (50.36%), Space: 54.6 MB (59.18%) - LeetHub

--- JavaScript/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ JavaScript/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -2,15 +2,12 @@
  * @param {number[]} prices
  * @return {number}
  */
-var 
-
-maxProfit = function(prices) {
-   let n=prices.length;
-   let maxiProfite =0 ;
-   for(let i=1;i<n;i++){
-if(prices[i] > prices[i-1]){
-    maxiProfite +=prices[i] - prices[i-1]
-}
-   }
-   return maxiProfite
+var maxProfit = function(prices) {
+    let maxi=0;
+    for(let i=1;i<prices.length;i++){
+      if(prices[i] > prices[i-1]){
+        maxi+=prices[i] - prices[i-1];
+      }
+    }
+    return maxi
 };
