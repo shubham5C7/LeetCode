153dd6ca442a9793882c114a7784670a6df7defa
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-04-05 12:36:56 +0530

    Time: 0 ms (100%), Space: 54.2 MB (62.97%) - LeetHub

--- JavaScript/0088-merge-sorted-array/0088-merge-sorted-array.js
+++ JavaScript/0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,24 +6,24 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let left=0,low=0;
     let res=[];
+    let left=0,low=0;
     while(left < m && low < n){
         if(nums1[left] < nums2[low]){
-            res.push(nums1[left++])
+            res.push(nums1[left++]);
         }else{
-            res.push(nums2[low++])
+              res.push(nums2[low++]);
         }
     }
     while(left < m){
-        res.push(nums1[left++]) 
+         res.push(nums1[left++]);
     }
-    while(low < n){
-       res.push(nums2[low++])
+      while(low < n){
+         res.push(nums2[low++]);
     }
 
-   for(let i=0;i<res.length;i++){
-    nums1[i] = res[i]
-   }
- return nums1
+    for(let i=0;i<res.length;i++){
+        nums1[i] = res[i]
+    }
+    return nums1
 };
