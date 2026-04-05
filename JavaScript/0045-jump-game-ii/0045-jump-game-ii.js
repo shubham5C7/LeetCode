13d775977315cd2d1c55163cff5b28b29ca306cc
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-04-05 21:34:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- JavaScript/0045-jump-game-ii/0045-jump-game-ii.js
+++ JavaScript/0045-jump-game-ii/0045-jump-game-ii.js
@@ -6,9 +6,10 @@ var jump = function(nums) {
     let jump=0,currEnd=0,fast=0;
     for(let i=0;i<nums.length-1;i++){
         fast=Math.max(fast,i+nums[i]);
-        if(i === currEnd)
+        if(i===currEnd){
         jump++;
-        currEnd = fast;
+        currEnd = fast
+        }
     }
-    return jump;
+    return jump
 };
