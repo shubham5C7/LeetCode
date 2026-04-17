28d41672d9297ea78aa73ed5a2a4591a7f9b2180
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-04-17 21:16:56 +0530

    Time: 0 ms (100%), Space: 55.6 MB (47.01%) - LeetHub

--- JavaScript/0202-happy-number/0202-happy-number.js
+++ JavaScript/0202-happy-number/0202-happy-number.js
@@ -3,20 +3,20 @@
  * @return {boolean}
  */
  const getNum = function(n){
-    let sum=0;
+    let sum = 0;
     while(n!=0){
-        let digit = n%10;
-        sum+=digit*digit;
+        let dig = n%10;
+        sum+=dig * dig;
         n=Math.floor(n/10);
     }
     return sum;
  }
 var isHappy = function(n) {
-  let slow = n;
-  let fast = getNum(n);
-  while(fast !== 1 && slow !== fast){
-    slow = getNum(slow);
-    fast = getNum(getNum(fast))
-  }
-  return fast === 1;
+    let slow = n;
+    let fast = getNum(n);
+    while(fast !== 1 && slow !== fast){
+        slow = getNum(slow);
+        fast = getNum(getNum(fast))
+    }
+   return fast === 1
 };
