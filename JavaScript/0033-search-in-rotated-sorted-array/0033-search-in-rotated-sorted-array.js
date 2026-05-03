64437ebe1b18b9de135498e7712fd70bf583501c
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-05-03 22:29:55 +0530

    Time: 0 ms (100%), Space: 53.7 MB (64.55%) - LeetHub

--- JavaScript/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ JavaScript/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,10 +4,28 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for(let i=0;i<nums.length;i++){
-        if(nums[i] === target){
-            return i
+    let left = 0 ,right = nums.length-1;
+    while(left <= right){
+        let mid = Math.floor((left+right)/2);
+ 
+        if(nums[mid] === target) return mid;
+
+        if(nums[left] <= nums[mid]){
+            if(nums[left] <=target && nums[mid] > target){
+                right = mid -1;
+            }else{
+                left = mid +1 ;
+            }
         }
+
+        else {
+            if(nums[mid] < target && target <= nums[right]){
+                left = mid+1
+            }else{
+                right = mid- 1
+            }
+        }
+      
     }
-    return -1
+    return -1;
 };
