df0f98a5535cfa166400df2c8fcf666b690224d3
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-05-03 23:03:58 +0530

    Time: 0 ms (100%), Space: 53.7 MB (67.71%) - LeetHub

--- JavaScript/0035-search-insert-position/0035-search-insert-position.js
+++ JavaScript/0035-search-insert-position/0035-search-insert-position.js
@@ -4,10 +4,17 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    for(let i=0;i<nums.length;i++){
-        if(nums[i] >= target){
-            return i;
+    let left = 0 ,right = nums.length-1;
+    while(left <= right){
+        let mid = Math.floor((left+right)/2);
+
+        if(nums[mid] === target) return mid ;
+
+        if(nums[mid] > target){
+            right = mid -1;
+        }else{
+            left = mid +1 
         }
     }
-    return nums.length;
+return left
 };
