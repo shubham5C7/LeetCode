a826f7a0c3c270209cb1686de69924aecbf02e40
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-05-03 22:57:49 +0530

    Time: 0 ms (100%), Space: 53.6 MB (96.34%) - LeetHub

--- JavaScript/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ JavaScript/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,16 +4,39 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    let n=nums.length;
-    let first=-1;
-    let last=-1;
-    for(let i=0;i<n;i++){
-    if(nums[i] ===target){
-        if(first === -1){
-            first=i;
-        }
-            last=i;
+    
+    function first(nums,target){
+   let left=0,right=nums.length-1,ans=-1;
+
+   while(left <= right){
+ let mid = Math.floor((left+right)/2);
+  if(nums[mid] === target){
+    ans = mid;
+    right = mid -1 ;
+  }else if(nums[mid] < target){
+    left = mid +1;
+  }else {
+    right = mid -1
+  }
+   } 
+   return ans
     }
+
+      function last(nums,target){
+         let left=0,right=nums.length-1,ans=-1;
+            while(left <= right){
+     let mid = Math.floor((left+right)/2);
+
+ if( nums[mid] === target){
+    ans = mid;
+    left = mid +1;
+ }else if(nums[mid] < target){
+     left = mid +1
+ }else{
+    right = mid -1
+ }
+            }
+         return ans
     }
-    return [first,last];
+ return [first(nums, target), last(nums, target)];
 };
