2873a2a69b30beb90309bfcf57d17be1afdbd022
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-05-04 12:15:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- JavaScript/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ JavaScript/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,39 +4,40 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    
-    function first(nums,target){
-   let left=0,right=nums.length-1,ans=-1;
+    function first(nums, target){
+    let left=0,right=nums.length-1,ans=-1;
+    while(left <= right){
+        let mid = Math.floor((left+right)/2);
+        if(nums[mid] === target){
+            ans = mid;
+            right = mid -1
+        }
 
-   while(left <= right){
- let mid = Math.floor((left+right)/2);
-  if(nums[mid] === target){
-    ans = mid;
-    right = mid -1 ;
-  }else if(nums[mid] < target){
-    left = mid +1;
-  }else {
-    right = mid -1
-  }
-   } 
-   return ans
+      else  if(nums[mid] <= target){
+            left = mid +1;
+        }else{
+            right = mid -1
+        }
     }
-
-      function last(nums,target){
+    return ans;
+    }
+      function last(nums, target){
          let left=0,right=nums.length-1,ans=-1;
-            while(left <= right){
-     let mid = Math.floor((left+right)/2);
-
- if( nums[mid] === target){
-    ans = mid;
-    left = mid +1;
- }else if(nums[mid] < target){
-     left = mid +1
- }else{
-    right = mid -1
- }
-            }
-         return ans
+             while(left <= right){
+        let mid = Math.floor((left+right)/2);
+   if(nums[mid] === target){
+            ans = mid;
+            left = mid+ 1
+        }
+  else if(nums[mid] > target){
+            right = mid -1
+        }else{
+            left = mid +1;
+        }
     }
- return [first(nums, target), last(nums, target)];
+
+        return ans
+    }
+
+    return [first(nums, target) , last(nums, target)]
 };
