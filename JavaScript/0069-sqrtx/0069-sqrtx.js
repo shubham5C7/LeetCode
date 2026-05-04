f46050b0416ebe56aac74053c71a2dacb18271f9
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-05-04 12:21:24 +0530

    Time: 0 ms (100%), Space: 55.7 MB (62.02%) - LeetHub

--- JavaScript/0069-sqrtx/0069-sqrtx.js
+++ JavaScript/0069-sqrtx/0069-sqrtx.js
@@ -4,17 +4,20 @@
  */
 var mySqrt = function(x) {
     let left = 0,right = x;
-    let ans = -1;
+    let ans = -1
     while(left <= right){
-        let mid = Math.floor((left+right)/2);
-        if(mid * mid === x) return mid;
+  let mid = Math.floor((left+right)/2);
 
-        if(mid * mid < x){
-            ans = mid;
-            left = mid +1 ;
-        }else{
-            right = mid -1;
-        }
+  if(mid * mid === x){
+    return mid
+  }
+  else if(mid * mid < x){
+  ans = mid;
+  left= mid +1
+  }else{
+    right = mid -1
+  }
+
     }
-    return ans;
+    return ans
 };
