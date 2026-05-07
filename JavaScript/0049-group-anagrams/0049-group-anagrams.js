78cb7c78f90b28ba7e64771fded20b9c381c4c80
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-05-07 11:29:12 +0530

    Time: 27 ms (78.17%), Space: 68.1 MB (45.1%) - LeetHub

---
+++ JavaScript/0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let map = new Map();
+
+    for(let str of strs){
+        let key = str.split("").sort().join("");
+
+        if(!map.has(key)){
+            map.set(key,[])
+        }
+        map.get(key).push(str)
+    }
+    return Array.from(map.values())
+};
