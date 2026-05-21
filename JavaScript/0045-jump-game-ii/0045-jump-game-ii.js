57e2f1417784ff253ab7c84fad6e463d62e7a135
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-05-21 11:14:00 +0530

    Time: 1 ms (84.84%), Space: 54.6 MB (69.68%) - LeetHub

--- JavaScript/0045-jump-game-ii/0045-jump-game-ii.js
+++ JavaScript/0045-jump-game-ii/0045-jump-game-ii.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var jump = function(nums) {
-    let jump=0,fast=0,currIdx=0;
+    let jump=0,currIdx=0,fast=0;
     for(let i=0;i<nums.length-1;i++){
         fast=Math.max(fast,i+nums[i]);
-        if(i === currIdx){
+        if(i===currIdx){
             jump++;
             currIdx = fast
         }
