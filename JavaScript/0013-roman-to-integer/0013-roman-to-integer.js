bf749ce7d690070465cae2bf7f6e5a1540f18769
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-05-21 15:11:35 +0530

    Time: 6 ms (41.41%), Space: 62.3 MB (17.92%) - LeetHub

--- JavaScript/0013-roman-to-integer/0013-roman-to-integer.js
+++ JavaScript/0013-roman-to-integer/0013-roman-to-integer.js
@@ -3,25 +3,26 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    let mpp = new Map([
-        ["I",1],
-        ["V",5],
-        ["X",10],
-        ["L",50],
-        ["C",100],
-        ["D",500],
-        ["M",1000]
-        ]);
-        let sum=0;
-        for(let i=0;i<s.length;i++){
-            let curr = mpp.get(s[i]);
-            let next=mpp.get(s[i+1]);
+    let sum = 0;
+ let map = new Map([
+    ["I",1],
+    ["V",5],
+    ["X",10],
+    ["L",50],
+    ["C",100],
+    ["D",500],
+    ["M",1000]
+]);
 
-            if(curr && curr < next){
-                sum-=curr
-            }else{
-                sum+=curr;
-            }
+    for(let i=0;i<s.length;i++){
+        let curr = map.get(s[i])
+        let next = map.get(s[i+1]);
+
+        if(curr && curr < next){
+           sum-=curr
+        }else{
+            sum+=curr
         }
-        return sum
+    }
+    return sum
 };
