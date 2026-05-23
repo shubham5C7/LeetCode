370e987b48f1d4245af1dbd4f945904c5bbb3aa6
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-05-23 15:16:06 +0530

    Time: 21 ms (44.14%), Space: 61.5 MB (93.52%) - LeetHub

--- JavaScript/0221-maximal-square/0221-maximal-square.js
+++ JavaScript/0221-maximal-square/0221-maximal-square.js
@@ -4,14 +4,15 @@
  */
 var maximalSquare = function(matrix) {
     let maxi=0;
+
     for(let i=0;i<matrix.length;i++){
         for(let j=0;j<matrix[0].length;j++){
             if(matrix[i][j] === "0") continue;
             if(i>0 && j>0){
-                matrix[i][j] = Math.min(matrix[i-1][j],matrix[i][j-1],matrix[i-1][j-1]) + 1;
+                matrix[i][j] =Math.min(matrix[i-1][j],matrix[i][j-1],matrix[i-1][j-1])+1;
             }
-                maxi=Math.max(matrix[i][j],maxi)
+            maxi=Math.max(matrix[i][j],maxi)
         }
     }
-    return maxi**2
+    return  maxi**2
 };
