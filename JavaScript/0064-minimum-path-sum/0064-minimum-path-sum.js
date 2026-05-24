65c9169299363a094e165739a7b82108f0cd8107
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-05-24 15:26:02 +0530

    Time: 6 ms (54.42%), Space: 55.1 MB (92.86%) - LeetHub

--- JavaScript/0064-minimum-path-sum/0064-minimum-path-sum.js
+++ JavaScript/0064-minimum-path-sum/0064-minimum-path-sum.js
@@ -3,20 +3,20 @@
  * @return {number}
  */
 var minPathSum = function(grid) {
-        let m = grid.length;
-    let n = grid[0].length;
-
+    let m = grid.length
+    let n =grid[0].length
     for(let j=1;j<n;j++){
-        grid[0][j] += grid[0][j-1];
+        grid[0][j] += grid[0][j-1]; 
+    }
+    for(let i=1;i<m;i++){
+          grid[i][0] += grid[i-1][0]; 
     }
 
-    for(let i=1;i<m;i++){
-           grid[i][0] += grid[i-1][0];
+ for(let i=1;i<m;i++){
+    for(let j=1;j<n;j++){
+        grid[i][j] += Math.min(grid[i-1][j],grid[i][j-1])
     }
-  for(let j=1;j<n;j++){
-       for(let i=1;i<m;i++){
-        grid[i][j] += Math.min(grid[i][j-1],grid[i-1][j])
-       }
-  }
-  return grid[m-1][n-1]
+ }
+
+return grid[m-1][n-1]
 };
