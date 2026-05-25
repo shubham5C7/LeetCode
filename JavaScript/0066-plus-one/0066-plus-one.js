89ad77aea49b2b134aaf5de73a52315313f70ede
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-05-26 00:30:26 +0530

    Time: 0 ms (100%), Space: 53.7 MB (46.84%) - LeetHub

--- JavaScript/0066-plus-one/0066-plus-one.js
+++ JavaScript/0066-plus-one/0066-plus-one.js
@@ -3,14 +3,14 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    let n = digits.length;
+    let n=digits.length;
     for(let i=n-1;i>=0;i--){
         if(digits[i] < 9){
             digits[i]++;
             return digits;
         }
-        digits[i] = 0;
+        digits[i]=0;
     }
     digits.unshift(1);
-    return digits;
+    return digits
 };
