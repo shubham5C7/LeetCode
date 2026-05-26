51a3f367b8992be36051024629770e19a38f3941
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-05-26 23:05:00 +0530

    Time: 0 ms (100%), Space: 54.8 MB (52.29%) - LeetHub

--- JavaScript/0001-two-sum/0001-two-sum.js
+++ JavaScript/0001-two-sum/0001-two-sum.js
@@ -4,20 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let arrs = nums.map((num,idx)=>[num,idx]);
+    let mpp= {};
 
-    let n= arrs.length;
-    arrs.sort((a,b)=>a[0]-b[0]);
-    let left = 0,right = n-1;
-    while(left <right){
-    let sum = arrs[left][0]+arrs[right][0];
-     if(sum === target){
-        return [arrs[left][1],arrs[right][1]];
-     }  
-     else if(sum < target){
-        left++
-     }else{
-        right --
-     }
+    for(let i=0;i<nums.length;i++){
+        let third = target - nums[i];
+        if(third in mpp){
+            return [mpp[third],i];
+        }
+        mpp[nums[i]] = i;
     }
+    return nums
 };
