d3655ae918969f9220e4e4ccbcdd846fce6128c3
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-06-04 16:58:05 +0530

    Time: 2 ms (73.08%), Space: 56.1 MB (80.42%) - LeetHub

--- JavaScript/0169-majority-element/0169-majority-element.js
+++ JavaScript/0169-majority-element/0169-majority-element.js
@@ -6,13 +6,14 @@ var majorityElement = function(nums) {
     let cnt=0;
     let maj = 0;
     for(let num of nums){
-        if(cnt === 0){
-            maj = num;
-            cnt = 1;
-        }else if(maj === num){
-            cnt++;
-        }else
-        cnt--;
+     if(cnt === 0){
+        maj=num
+        cnt=1;
+     }else if(maj === num){
+        cnt++
+     }else{
+        cnt--
+     }
     }
     return maj
 };
