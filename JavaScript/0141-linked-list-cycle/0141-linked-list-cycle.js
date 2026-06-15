eeb1603c2129c7e96c7a426474c46f618182bb63
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-06-15 18:06:24 +0530

    Time: 53 ms (63.56%), Space: 59.4 MB (43.08%) - LeetHub

--- JavaScript/0141-linked-list-cycle/0141-linked-list-cycle.js
+++ JavaScript/0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -17,7 +17,7 @@ var hasCycle = function(head) {
     while(fast !== null && fast.next !== null){
      slow = slow.next;
      fast = fast.next.next
-     if(slow === fast) return true
+     if(slow === fast) return true;
     }
-    return false
+    return false;
 };
