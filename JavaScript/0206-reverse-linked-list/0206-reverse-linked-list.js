7e7244178784188fb28e9c962874c629100db306
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-06-16 00:32:12 +0530

    Time: 0 ms (100%), Space: 57.9 MB (40.48%) - LeetHub

--- JavaScript/0206-reverse-linked-list/0206-reverse-linked-list.js
+++ JavaScript/0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -13,10 +13,10 @@ var reverseList = function(head) {
     let curr = head;
     let prev = null;
     while(curr !== null){
-        let next = curr.next;
-        curr.next = prev
-        prev = curr;
-        curr = next
+        let next = curr.next
+        curr.next = prev;
+        prev = curr
+        curr = next 
     }
     return prev
 };
