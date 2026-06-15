11f9aa59d88ddaee556482185bef179b5a363281
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-06-16 00:38:27 +0530

    Time: 0 ms (100%), Space: 53.9 MB (30.81%) - LeetHub

--- JavaScript/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ JavaScript/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -10,11 +10,11 @@
  * @return {ListNode}
  */
 var middleNode = function(head) {
-    let fast = head;
     let slow = head;
-    while(fast !== null && fast.next !== null){
-        fast = fast.next.next
-        slow=slow.next
+    let fast = head;
+    while(fast != null && fast.next !== null){
+        fast = fast.next.next;
+        slow = slow .next
     }
     return slow
 };
