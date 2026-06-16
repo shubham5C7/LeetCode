24049944780d4352e93d57243d09f819c0ab8580
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-06-16 23:27:45 +0530

    Time: 0 ms (100%), Space: 54.7 MB (7.51%) - LeetHub

--- JavaScript/0070-climbing-stairs/0070-climbing-stairs.js
+++ JavaScript/0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,12 +3,20 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    if(n<=2) return n;
-     let first=1,second=2;
-     for(let i=3;i<=n;i++){
-        let third = first+second;
-        first = second;
-        second = third
-     }
-     return second;
+    let dp = new Array(n+1).fill(-1);
+
+    function helper(i){
+    if(i === n) return 1;
+    if(i>n) return 0;
+
+    if(dp[i] !== -1) return dp[i]
+
+    let pick = helper(i+1);
+    let notpick = helper(i+2);
+
+      dp[i] = pick + notpick
+
+return dp[i]
+    }
+    return helper(0)
 };
