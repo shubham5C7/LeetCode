cd85eac2c87831b088a1afe142af147ef67ff24e
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-08-18 21:27:32 +0530

    Time: 0 ms (100%), Space: 54.6 MB (31.48%) - LeetHub

--- JavaScript/1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.js
+++ JavaScript/1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.js
@@ -3,20 +3,19 @@
  * @return {number}
  */
 var findMiddleIndex = function(nums) {
-    let totalSum =0;
-
+    let totalSum = 0;
     for(let num of nums){
         totalSum+=num
     }
     let leftSum = 0;
 
     for(let i=0;i<nums.length;i++){
-      let  rightSum = totalSum - leftSum -nums[i];
+        let rightSum = totalSum - leftSum - nums[i]
 
-      if(leftSum === rightSum){
-        return i
-      }
-      leftSum+=nums[i]
+        if(leftSum === rightSum){
+            return i
+        }
+        leftSum+=nums[i]
     }
     return -1
 };
