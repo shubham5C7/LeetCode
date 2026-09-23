f1dab310740ce2391d393f765e83bf12ede39449
shubham somavamshi <shubhamraopatil123@gmail.com>
2026-09-23 13:25:12 +0530

    Time: 0 ms (100%), Space: 53.9 MB (60.91%) - LeetHub

--- JavaScript/0059-spiral-matrix-ii/0059-spiral-matrix-ii.js
+++ JavaScript/0059-spiral-matrix-ii/0059-spiral-matrix-ii.js
@@ -3,33 +3,40 @@
  * @return {number[][]}
  */
 var generateMatrix = function(n) {
+    let top = 0
+    let bottom = n - 1
+    let left = 0
+    let right = n - 1
     let matrix = new Array(n).fill(0).map(()=>new Array(n).fill(0))
-   let left=0,right=n-1;
-   let top=0,bottom=n-1;
-   let num=1
-   while(left <= right && top <= bottom){
-    for(let i=left;i<=right;i++){
-       matrix[top][i]=num++;
-    }
-    top++;
-
-    for(let i=top;i<=bottom;i++){
-        matrix[i][right]=num++;
-    }
-    right--;
-
-    if(top<=bottom){
-        for(let i=right;i>=left;i--){
-            matrix[bottom][i]=num++
+    let num = 1
+    while(top <= bottom && left <= right){
+        for(let i = left; i<= right; i++){
+            matrix[top][i] = num
+            num++
         }
-        bottom--;
-    }
-    if(left<=right){
-        for(let i=bottom;i>=top;i--){
-            matrix[i][left]=num++
+        top++
+        
+        for(let i = top; i<=bottom; i++){
+            matrix[i][right] = num
+            num ++
         }
-        left++
+        right--
+
+        if(top <= bottom){
+            for(let i = right; i >= left; i--){
+                matrix[bottom][i] = num 
+                num ++
+            }
+            bottom --
+        }
+
+        if(left <= right){
+            for(let i = bottom; i >= top; i--){
+                matrix[i][left] = num
+                num ++
+            }
+            left ++
+        }
     }
-   }
-   return matrix
+    return matrix
 };
